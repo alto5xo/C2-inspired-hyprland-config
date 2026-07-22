113b7d299b5edcfb678aa17afe4fbd1a79aa06d4
Tuna <58302747+alto5xo@users.noreply.github.com>
2026-07-23 02:36:54 +0300

    Create colors.lua

---
+++ colors.lua
@@ -0,0 +1,6 @@
+COLOR_ACTIVE_WHITE   = "rgb(ffffff)"
+COLOR_LIGHT_GREY     = "rgb(a0a0a0)"
+COLOR_MID_GREY       = "rgb(555555)"
+COLOR_BORDER_GREY    = "rgb(151515)"
+
+COLOR_ALERT_RED      = "rgb(ff3333)"
